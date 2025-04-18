dc9a43dc17fbc85bbc1ace1250e6ba2c4ec063ce
Martín Fenocchio <fenomartin6@gmail.com>
2025-04-18 00:02:39 -0300

    feat: add Sentry integration for error tracking and update Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,23 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "rapidamente",
-  plugins: [react()],
+
+  plugins: [react(), sentryVitePlugin({
+    org: "limboteams",
+    project: "rapidamente"
+  })],
+
   resolve: {
     alias: {
       src: "/src",
     },
   },
+
+  build: {
+    sourcemap: true
+  }
 });
